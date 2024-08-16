42fd6d4abd63d8b29427d30d594dc81ec3f87587
Admin <Admin@Jyoti>
2024-08-16 16:59:38 +0530

    jenkins first push

---
+++ JenkinsProject/src/test/java/jenkinsPackage/Jenkins_Script_Test.java
@@ -0,0 +1,18 @@
+package jenkinsPackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Jenkins_Script_Test {
+	@Test
+	public void launch()
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demowebshop.tricentis.com/");
+		Reporter.log("Jenkins excuted");
+	}
+
+}
